ff16b34f4b0cc0be4bdfafe305310ba2607acfff
Souvik Kundu <kivtech88@gmail.com>
2022-02-08 13:32:59 +0530

    Update multidimensionalArray.js

--- multidimensionalArray.js
+++ multidimensionalArray.js
@@ -11,7 +11,7 @@ let car = [
 ];
 // now it iterates through the multidimensional array and prints the values on screen respectively
 for(var i=0; i < car.length; i++) {
-    for(var j=0; j < car[i].length; j++) {
+    for(var j=0; j < car[i].length; j++) { // *j* is an iterator 
         console.log(car[i][j]);
     }
     console.log("the row ends here"); // every time a row ends and moves to the next row, it prints "row ends here"
