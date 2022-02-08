7a0d44b6e8236216bf1ea6c7bb7c5e8c70bd5339
Souvik Kundu <kivtech88@gmail.com>
2022-02-08 16:15:20 +0530

    Update iteratingObjects.js

--- iteratingObjects.js
+++ iteratingObjects.js
@@ -1,3 +1,7 @@
 /*
-    Objects are 
+    Objects are an unordered collection of data, the data item is a value 
+    reference by a key / property name.
+
+    To access the value, we use the property name (key) to refer to the data 
+    item.
 */
