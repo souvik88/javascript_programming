bb91ac5cff32b23967f08e3e1d468731abe1a730
Souvik Kundu <kivtech88@gmail.com>
2022-02-09 20:07:00 +0530

    Create countingPopulation.js

---
+++ countingPopulation.js
@@ -0,0 +1,19 @@
+/*
+Problem statement:
+
+You are given one variable named raw_array, assigned to a two-dimensional 
+array consisting of objects. Each object has the following properties:
+
+name: This is assigned to a string with the value of the city’s name 
+in which the population resides. Example, {name: "Sydney"}
+
+population: This is assigned to a number with the value of the quantity 
+of the residing population. Example, {population: 1250000}
+
+temp: This is assigned to a number with the value of the temperature 
+(in °C) of the region. Example, {temp: 3.0}
+
+currency: This is assigned to a string with the value of the currency 
+code of the city in which the population resides. Example, {currency: "AUD"}
+*/
+
