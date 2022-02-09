001b1939787e61a485638add19053c4fce9dfaa0
Souvik Kundu <kivtech88@gmail.com>
2022-02-09 19:57:59 +0530

    Update iteratingObjects.js

--- iteratingObjects.js
+++ iteratingObjects.js
@@ -1,3 +1,9 @@
+/*
+variable places is assigned to the keys of the object countries
+
+Using the key to access the value of the object countries, and then the
+corresponding values of the object
+*/
 var countries = {'North America': 'USA', 'Europe': 'UK', 'Asia': 'Japan'};
 
 for(var places in countries) {
@@ -5,3 +11,9 @@ for(var places in countries) {
     console.log(`Continent ${places} and Country ${countries[places]}`);
     console.log("\n");
 }
+
+/*
+ for..in loop returns the list of keys on the object being iterated
+
+ for..of lopop return the list of values on the object being iterated
+*/
