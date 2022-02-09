1049092bad5fb63a5b1fc53047380d4d582e66a8
Souvik Kundu <kivtech88@gmail.com>
2022-02-10 02:50:00 +0530

    Update countingPopulation.js

--- countingPopulation.js
+++ countingPopulation.js
@@ -1,33 +1,34 @@
-/*
-Task#
+// this is not my solution however the solution is correct
 
-A demographist wants to find the total population within a demographic. 
-This demographic is in a region with a temperature less than or equal to 10°C. 
-However, with a limited programming background, 
-the demographist has asked for your help!
+// Task#
 
-Your task: is to find the sum of the population in a 
-two-dimensional array of objects which reside in a region of 
-temperature less than or equal to 10°C. Each object consists of the 
-region information.
+// A demographist wants to find the total population within a demographic. 
+// This demographic is in a region with a temperature less than or equal to 10°C. 
+// However, with a limited programming background, 
+// the demographist has asked for your help!
 
-Problem statement:
+// Your task: is to find the sum of the population in a 
+// two-dimensional array of objects which reside in a region of 
+// temperature less than or equal to 10°C. Each object consists of the 
+// region information.
 
-You are given one variable named raw_array, assigned to a two-dimensional 
-array consisting of objects. Each object has the following properties:
+// Problem statement:
 
-name: This is assigned to a string with the value of the city’s name 
-in which the population resides. Example, {name: "Sydney"}
+// You are given one variable named raw_array, assigned to a two-dimensional 
+// array consisting of objects. Each object has the following properties:
 
-population: This is assigned to a number with the value of the quantity 
-of the residing population. Example, {population: 1250000}
+// name: This is assigned to a string with the value of the city’s name 
+// in which the population resides. Example, {name: "Sydney"}
 
-temp: This is assigned to a number with the value of the temperature 
-(in °C) of the region. Example, {temp: 3.0}
+// population: This is assigned to a number with the value of the quantity 
+// of the residing population. Example, {population: 1250000}
 
-currency: This is assigned to a string with the value of the currency 
-code of the city in which the population resides. Example, {currency: "AUD"}
-*/
+// temp: This is assigned to a number with the value of the temperature 
+// (in °C) of the region. Example, {temp: 3.0}
+
+// currency: This is assigned to a string with the value of the currency 
+// code of the city in which the population resides. Example, {currency: "AUD"}
+
 
 // solution below
 
