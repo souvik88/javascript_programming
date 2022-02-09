227f6755d64bace4eb77e5a64c8ae3a2712f60f2
Souvik Kundu <kivtech88@gmail.com>
2022-02-09 20:04:07 +0530

    Update iteratingObjects.js

--- iteratingObjects.js
+++ iteratingObjects.js
@@ -7,13 +7,21 @@ corresponding values of the object
 var countries = {'North America': 'USA', 'Europe': 'UK', 'Asia': 'Japan'};
 
 for(var places in countries) {
-    console.log("\n");
+    console.log(" ");
     console.log(`Continent ${places} and Country ${countries[places]}`);
-    console.log("\n");
+    console.log(" ");
 }
 
-/*
- for..in loop returns the list of keys on the object being iterated
+// output
+// Continent North America and Country USA
 
- for..of lopop return the list of values on the object being iterated
+// Continent Europe and Country UK
+
+// Continent Asia and Country Japan
+
+/*
+notes
+    for..in loop returns the list of keys on the object being iterated
+
+    for..of lopop return the list of values on the object being iterated
 */
