52acfa229facc52b41072ee3b38fd6cbe1ac04ec
Souvik Kundu <kivtech88@gmail.com>
2022-02-14 00:51:40 +0530

    Update arrowFunctions.js

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,5 +1,15 @@
 // example of arrow function
 
+// single line function as arrow function
 // var sum = (a, b) => a + b;
 // console.log(sum(1,2));
+
+// regular function as arrow function
+// var sum = (a, b, c) => {
+//     console.log('Adding 3 values');
+//     return (a + b + c);
+// }
+
+// console.log(sum(1,1,1));
+
 
