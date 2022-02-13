16dd0155540cbacf81989797a63514a136028405
Souvik Kundu <kivtech88@gmail.com>
2022-02-14 03:08:37 +0530

    Update functionAsArgument.js

--- functionAsArgument.js
+++ functionAsArgument.js
@@ -1,8 +1,19 @@
 // passing function as an argument
 
-function test0() {
-    console.log('testing2');
-}
+// function test0() {
+//     console.log('testing2');
+// }
+// function test1(test2) {
+//     console.log('testing0');
+//     console.log('testing1');
+//     test2();
+//     console.log('testing3');
+//     console.log('testing4');
+// }
+// test1(test0);
+
+// passing an anonymous function as an argument
+
 function test1(test2) {
     console.log('testing0');
     console.log('testing1');
@@ -10,4 +21,6 @@ function test1(test2) {
     console.log('testing3');
     console.log('testing4');
 }
-test1(test0);
+test1(function() {
+    console.log("hello world");
+});
