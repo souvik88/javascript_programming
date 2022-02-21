46136d5a0ed7d83b51f235b9b0b58a3e34ce3d7a
Souvik Kundu <kivtech88@gmail.com>
2022-02-21 17:02:32 +0530

    commit

---
+++ filterMethod.js
@@ -0,0 +1,3 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var arr1 = arr.filter(x => x >= 8);
+console.log(arr1, typeof arr1);
---
+++ forEach.js
@@ -0,0 +1,5 @@
+var arr = [10, 20, 30, 40, 50];
+arr.forEach((val, ind, array) => {
+    console.log("Values:", val, " Index:", ind, " arr:", array);
+});
+console.log(arr);
