1d585199eb726f9f08c9ede5ff3878372eb3c83c
Souvik Kundu <kivtech88@gmail.com>
2022-06-04 16:33:03 +0530

    Update generators.js

--- generators.js
+++ generators.js
@@ -1,14 +1,30 @@
 // javascript generators
 
-function* menuList() {
-    yield 'chicken';
-    yield 'rice';
-    yield 'dessert';
+// simple javascript generator
+// function* menuList() {
+//     yield 'chicken';
+//     yield 'rice';
+//     yield 'dessert';
+// }
+
+// const lunch = menuList();
+
+// console.log(lunch.next());
+// console.log(lunch.next());
+// console.log(lunch.next());
+// console.log(lunch.next());
+/*******************************/
+
+// looping over an array with a generator
+const cars = ["mercedes", "bmw", "audi", "jeep"];
+// creating a loop generator
+function* carLoop(arr) {
+    for(const car of arr) {
+        yield `I like this car: ${car}`;
+    }
 }
-
-const lunch = menuList();
-
-console.log(lunch.next());
-console.log(lunch.next());
-console.log(lunch.next());
-console.log(lunch.next());
+const carGenerator = carLoop(cars);
+console.log(carGenerator.next());
+console.log(carGenerator.next());
+console.log(carGenerator.next());
+console.log(carGenerator.next().value);
