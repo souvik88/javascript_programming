b23e56dfb5fa544fe0a1d413e70b844f199ab6e1
Souvik Kundu <kivtech88@gmail.com>
2022-06-04 17:15:16 +0530

    Update generators.js

--- generators.js
+++ generators.js
@@ -16,15 +16,34 @@
 /*******************************/
 
 // create a loop generator
-const cars = ["mercedes", "bmw", "audi", "jeep"];
-// creating a loop generator
-function* carLoop(arr) {
-    for(const car of arr) {
-        yield `I like this car: ${car}`;
+// const cars = ["mercedes", "bmw", "audi", "jeep"];
+// // creating a loop generator
+// function* carLoop(arr) {
+//     for(const car of arr) {
+//         yield `I like this car: ${car}`;
+//     }
+// }
+// const carGenerator = carLoop(cars);
+// console.log(carGenerator.next());
+// console.log(carGenerator.next());
+// console.log(carGenerator.next());
+// console.log(carGenerator.next().value);
+
+/***********************************/
+
+// catching errors with .throw() using generator
+
+function* test() {
+    try {
+        yield "test0";
+        yield "test1";
+        yield "test2";
+        yield "test3";
+    }
+    catch(err) {
+        console.log(`Error: ${err}`);
     }
 }
-const carGenerator = carLoop(cars);
-console.log(carGenerator.next());
-console.log(carGenerator.next());
-console.log(carGenerator.next());
-console.log(carGenerator.next().value);
+const testGenerator = test();
+console.log(testGenerator.next());
+console.log(testGenerator.throw('caught an error'));
